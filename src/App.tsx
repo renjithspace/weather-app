e3cc25e4475c995664c24aaeda7cd6c956485460
renjithspace <renjithrajeevvk@gmail.com>
2021-04-22 13:10:22 +0530

    Setup material ui and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Container, createMuiTheme, ThemeProvider } from '@material-ui/core'
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: 'Inter, sans-serif'
+  }
+})
 
 function App () {
-  return <div></div>
+  return (
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="md">
+      </Container>
+    </ThemeProvider>
+  )
 }
 
 export default App
